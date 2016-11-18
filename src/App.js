a1493e41bf706d12e19dad3ed76845559bfc3eb2
Gaizka Allende <gaizka@allende.org>
2016-11-18 20:29:48 +0100

    made char very reusable

--- src/App.js
+++ src/App.js
@@ -26,53 +26,15 @@ const Bar = ({cssRule, label, x, y, width, height}) => {
 Bar.propTypes = {
   className: React.PropTypes.string,
   x: React.PropTypes.number,
-  y: React.PropTypes.number,
   width: React.PropTypes.number,
-  height: React.PropTypes.number
+  height: React.PropTypes.number,
+  axisValues: React.PropTypes.array,
+  axisSpacing: React.PropTypes.number
+
 };
 
 Bar.defaultProps = {
-    width: 30
-}
-
-
-const Bars = ({desiredAmount, currentAmount, axisValues, axisSpacing}) => {
-
-    const desiredBarHeight = desiredAmount / 1000 ;
-    const currentBarHeight = currentAmount / 1000 ;
-
-
-    const y = axisValues
-        .map((y, i) => ({
-            y: (i+1)*axisSpacing,
-            value: y
-        })).filter(axisLine => axisLine.value === '0')[0].y;
-
-    return (
-        <g className="liquidity-chart__bars">
-            <Bar
-                cssRule={'desired'}
-                label={desiredAmount.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
-                x={75}
-                y={y - desiredBarHeight}
-                height={desiredBarHeight} />
-            <Bar
-                cssRule={'current'}
-                label={currentAmount.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
-                x={175}
-                y={y - currentBarHeight}
-                height={currentBarHeight} />
-
-        </g>
-    );
-}
-
-Bars.propTypes = {
-    axisValues: React.PropTypes.array,
-    axisSpacing: React.PropTypes.number
-}
-
-Bars.defaultProps = {
+    width: 30,
     axisSpacing: 25,
     axisValues: ['125T', '100T', '75T', '50T', '25T', '0', '-25T']
 }
@@ -131,40 +93,71 @@ Grid.propTypes = {
 
 Grid.defaultProps = {
     width: 300,
-    axisSpacing: 25,
-    axisValues: ['125T', '100T', '75T', '50T', '25T', '0', '-25T'],
     currencySign: '€'
 }
 
+const App = ({width, height, axisValues, axisSpacing}) => {
+    const desiredAmount = 40000;
+    const currentAmount = 1234.56;
+    const desiredBarHeight = desiredAmount / 1000 ;
+    const currentBarHeight = currentAmount / 1000 ;
 
-const Chart = ({id, bars, width, height}) => {
+    const barYCoordinate = axisValues
+        .map((y, i) => ({
+            y: (i+1)*axisSpacing,
+            value: y
+        }))
+        .filter(axisLine => axisLine.value === '0')[0].y;
+
 
     return (
-        <svg className={id} height={height} width={width}>
-            <Grid />
-        </svg>
-    )
-  }
-
-Chart.propTypes = {
-    id: React.PropTypes.string,
-    width: React.PropTypes.number,
-    height: React.PropTypes.number
-}
-
-
-Chart.defaultProps = {
-    width: 300,
-    height: 200
-}
-
-
-const App = () => {
-    const chartBars  = <Bars desiredAmount={desiredAmount} currentAmount={currentAmount} />
-    return (
-        <div className="placeholder">
-            <Chart id="liquidity-chart" desiredAmount={40000} currentAmount={1234.56} bars={chartBars}/>
+        <div>
+            <svg className="liquidity-chart" height={height} width={width}>
+                <Grid axisValues={axisValues} axisSpacing={axisSpacing}/>
+                <g className="liquidity-chart__bars">
+                    <Bar
+                        cssRule={'desired'}
+                        label={desiredAmount.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
+                        x={75}
+                        y={barYCoordinate - desiredBarHeight}
+                        height={desiredBarHeight} />
+                    <Bar
+                        cssRule={'current'}
+                        label={currentAmount.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
+                        x={175}
+                        y={barYCoordinate - currentBarHeight}
+                        height={currentBarHeight} />
+                </g>
+            </svg>
+            <svg className="liquidity-chart" height={height} width={width}>
+                <Grid axisValues={axisValues} axisSpacing={axisSpacing}/>
+                <g className="liquidity-chart__bars">
+                    <Bar
+                        cssRule={'desired'}
+                        label={desiredAmount.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}
+                        x={100}
+                        y={barYCoordinate - desiredBarHeight}
+                        height={desiredBarHeight} />
+                </g>
+            </svg>
         </div>
     )
 }
+
+App.propTypes = {
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
+    axisValues: React.PropTypes.array,
+    axisSpacing: React.PropTypes.number,
+}
+
+
+App.defaultProps = {
+    width: 300,
+    height: 200,
+    axisSpacing: 25,
+    axisValues: ['125T', '100T', '75T', '50T', '25T', '0', '-25T']
+}
+
+
 export default App;
