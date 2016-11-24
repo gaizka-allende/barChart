5b2fb1a3727591eda7354384b3e3d1ad295ad17c
Gaizka Allende <gaizka@allende.org>
2016-11-24 16:10:30 +0100

    changed styles

--- src/App.js
+++ src/App.js
@@ -13,20 +13,26 @@ const App = () => {
 
     return (
         <div className="charts">
-            <Chart css="chart-liquidity">
-                <Bar css={'chart__bar-desired'} value={46000} place={1/3} />
-                <Bar css={'chart__bar-current'} value={12234.56} place={2/3} />
-            </Chart>
-            <Chart css="chart-protection">
-                    <Bar css={'chart__bar-desired'} value={56874.25} place={1/3} />
-                    <StackedBars place={2/3} >
-                        <Bar css={'chart__bar-stacked-1'} value={38778} />
-                        <Bar css={'chart__bar-stacked-2'} value={9503.68} />
-                    </StackedBars>
-            </Chart>
-            <Chart css="chart-growth">
-                <Bar css={'chart__bar-desired'} value={-8750.23} place={1/2} />
-            </Chart>
+            <div className="chart-container">
+                <Chart css="chart-liquidity">
+                    <Bar css={'chart__bar-desired'} value={46000} place={1/3} />
+                    <Bar css={'chart__bar-current'} value={12234.56} place={2/3} />
+                </Chart>
+            </div>
+            <div className="chart-container">
+                <Chart css="chart-protection">
+                        <Bar css={'chart__bar-desired'} value={56874.25} place={1/3} />
+                        <StackedBars place={2/3} >
+                            <Bar css={'chart__bar-stacked-1'} value={38778} />
+                            <Bar css={'chart__bar-stacked-2'} value={9503.68} />
+                        </StackedBars>
+                </Chart>
+            </div>
+            <div className="chart-container">
+                <Chart css="chart-growth">
+                    <Bar css={'chart__bar-desired'} value={-8750.23} place={1/2} />
+                </Chart>
+            </div>    
         </div>
     )
 }
